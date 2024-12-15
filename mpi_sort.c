9190ac4097a7296a98e2ffbb90bbdbd43b45e5e3
annikola <annikolaprof@gmail.com>
2024-12-16 01:59:36 +0200

    First attempt to an instructions manual

--- mpi_sort.c
+++ mpi_sort.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define P 4
+#define P 8
 #define Q 4
+#define REPS 4 // log(P) + 1
 
 #define MAX_INTEGER 100
 #define MIN 0
 #define MAX 1
+#define ASCENT 0
+#define DESCENT 1
 
+int ipow(int base, int exp);
 int asc_compare(const void *a, const void *b);
 int desc_compare(const void *a, const void *b);
 void bitonic_swap(int *v1, int *v2, int idx);
@@ -16,122 +20,196 @@ void elbow_merge();
 
 int main(int argc, char *argv[]) {
 
-    int i, j, h, k;
+    int i, j, t, h, m, n, k, s;
     int A[P][Q + 1];
-    int uno_reverse;
+    int uno_reverse, pad, repetition, minmax_size, step;
+    int ascento[REPS][P];
+    char **minmax;
 
     srand(time(0));
 
-    uno_reverse = 0;
-    for (i = 0; i < P; i++) {
-        for (j = 0; j < Q; j++) {
-            A[i][j] = rand() % MAX_INTEGER + 1;
-        }
-        if ((i + uno_reverse) % 2 == 0) {
-            A[i][Q] = MIN;
-            uno_reverse = 0;
-        } else {
-            A[i][Q] = MAX;
-            uno_reverse = 1;
-        }
-    }
+    // uno_reverse = 0;
+    // for (i = 0; i < P; i++) {
+    //     for (j = 0; j < Q; j++) {
+    //         A[i][j] = rand() % MAX_INTEGER + 1;
+    //     }
+    //     if ((i + uno_reverse) % 2 == 0) {
+    //         A[i][Q] = MIN;
+    //         uno_reverse = 0;
+    //     } else {
+    //         A[i][Q] = MAX;
+    //         uno_reverse = 1;
+    //     }
+    // }
 
-    for (i = 0; i < P; i++) {
-        for (j = 0; j < Q; j++) {
-            printf("%d ", A[i][j]);
-        }
-        // printf("\n");
-    }
-    printf("\n");
-
-    for (i = 0; i < P; i++) {
-        if (i % 2 == 0) {
-            qsort(A[i], Q, sizeof(int), asc_compare);
-        } else {
-            qsort(A[i], Q, sizeof(int), desc_compare);
-        }
-    }
-
-    for (i = P / 2; i >= 1; i /= 2) {
-        for (j = 0; j < i; j++) {
-            for (h = 0; h < i; h++) {
-                // elbow_merge();
-                for (k = 0; k < Q; k++) {
-                    if ((A[j][k] > A[j + 1][k] && A[j][Q] == MIN) || (A[j][k] < A[j + 1][k] && A[j][Q] == MAX)) {
-                        bitonic_swap(A[j], A[j + 1], k);
-                    }
-                }
-                if (j == 0) { // HMM...
-                    qsort(A[j], Q, sizeof(int), asc_compare);
-                    A[j][Q] = MIN;
-                    qsort(A[j + 1], Q, sizeof(int), asc_compare);
-                    A[j + 1][Q] = MIN;
+    repetition = 0;
+    for (i = 1; i <= P; i *= 2) {
+        uno_reverse = 0;
+        for (j = 0; j < P; j += i) {
+            for (k = 0; k < i; k++) {
+                if (uno_reverse % 2 == 0) {
+                    ascento[repetition][j + k] = ASCENT;
                 } else {
-                    qsort(A[j], Q, sizeof(int), desc_compare);
-                    A[j][Q] = MAX;
-                    qsort(A[j + 1], Q, sizeof(int), desc_compare);
-                    A[j + 1][j] = MAX;
+                    ascento[repetition][j + k] = DESCENT;
                 }
             }
+            uno_reverse++;
         }
+        repetition++;
     }
 
-    for (j = 0; j < P; j += 2) {
-        for (k = 0; k < Q; k++) {
-            if ((A[j][k] > A[j + 1][k] && A[j][Q] == MIN) || (A[j][k] < A[j + 1][k] && A[j][Q] == MAX)) {
-                bitonic_swap(A[j], A[j + 1], k);
+    minmax_size = 0;
+    for (repetition = 0; repetition < REPS; repetition++) {
+        minmax_size += repetition;
+    }
+    printf("minmax_size: %d\n", minmax_size);
+
+    minmax = (char **)malloc(minmax_size * sizeof(char *));
+    for (repetition = 0; repetition < REPS; repetition++) {
+        minmax[repetition] = (char *)malloc(P * sizeof(char));
+    }
+
+    for (j = 0; j < REPS - 1; j++) {
+        for (i = 0; i < P; i++) {
+            if (!ascento[j][i] && !ascento[j + 1][i]) {
+                printf("MIN ");
+                minmax[j][i] = MIN;
+            } else if (!ascento[j][i] && ascento[j + 1][i]) {
+                printf("MAX ");
+                minmax[j][i] = MAX;
+            } else if (ascento[j][i] && !ascento[j + 1][i]) {
+                printf("MAX ");
+                minmax[j][i] = MAX;
+            } else {
+                printf("MIN ");
+                minmax[j][i] = MIN;
             }
         }
-        if (j == 0) { // HMM...
-            qsort(A[j], Q, sizeof(int), asc_compare);
-            A[j][Q] = MIN;
-            qsort(A[j + 1], Q, sizeof(int), asc_compare);
-            A[j + 1][Q] = MIN;
-        } else {
-            qsort(A[j], Q, sizeof(int), desc_compare);
-            A[j][Q] = MAX;
-            qsort(A[j + 1], Q, sizeof(int), desc_compare);
-            A[j + 1][j] = MAX;
-        }
-        // EDW PREPEI NA ANNEWTHOUN TA MIN MAX!!!
+        printf("\n");
     }
 
-    for (j = 0; j < P / 2; j++) {
-        for (k = 0; k < Q; k++) {
-            if (A[j][k] > A[j + 2][k] && A[j][Q] == MIN) {
-                bitonic_swap(A[j], A[j + 2], k);
+    for (repetition = 0; repetition < REPS; repetition++) {
+        step = ipow(2, repetition);
+        while ((step /= 2) >= 1) {
+            for (i = 0; i < P; i += step) {
+                printf("%d with %d\n", i, i + step);
             }
         }
-        if (j == 0) { // HMM...
-            A[j][Q] = MIN;
-            A[j + 2][Q] = MIN;
-        } else {
-            A[j][Q] = MAX;
-            A[j + 2][Q] = MAX;
-        }
-        // EDW PREPEI NA ANANEWTHOUN TA MIN MAX!!!
     }
 
-    for (j = 0; j < P; j += 2) {
-        for (k = 0; k < Q; k++) {
-            if (A[j][k] > A[j + 1][k] && A[j][Q] == MIN) {
-                bitonic_swap(A[j], A[j + 1], k);
-            }
-        }
-        qsort(A[j], Q, sizeof(int), asc_compare);
-        qsort(A[j + 1], Q, sizeof(int), asc_compare);
-    }
+    // for (i = 0; i < REPS; i++) {
+    //     for (j = 0; j < P; j++) {
+    //         printf("%d ", ascento[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
 
-    printf("\n");
-    for (i = 0; i < P; i++) {
-        for (j = 0; j < Q; j++) {
-            printf("%d ", A[i][j]);
-        }
-        // printf("\n");
-    }
-    printf("\n");
+    // for (i = 0; i < P; i++) {
+    //     if (i % 2 == 0) {
+    //         qsort(A[i], Q, sizeof(int), asc_compare);
+    //     } else {
+    //         qsort(A[i], Q, sizeof(int), desc_compare);
+    //     }
+    // }
+
+    // for (i = P / 2; i >= 1; i /= 2) {
+    //     uno_reverse = 0;
+    //     for (j = 0; j < i; j++) {
+    //         printf("j = %d\n", j);
+    //         pad = P / (2 * i); // the "elbow" length divided by 2
+    //         if ((j + uno_reverse) % 2 == 0) {
+    //             for (t = 0; t < pad; t++) {
+    //                 A[t + j * (P / i)][Q] = MIN;
+    //             }
+    //             for (t = pad; t < 2*pad; t++) {
+    //                 A[t + j * (P / i)][Q] = MAX;
+    //             }
+    //             uno_reverse = 0;
+    //         } else {
+    //             for (t = 0; t < pad; t++) {
+    //                 A[t + j * (P / i)][Q] = MAX;
+    //             }
+    //             for (t = pad; t < 2*pad; t++) {
+    //                 A[t + j * (P / i)][Q] = MIN;
+    //             }
+    //             uno_reverse = 1;
+    //         }
+
+    //         for (h = 0; h < pad; h++) {
+    //             for (k = 0; k < Q; k++) {
+    //                 if ((A[h + j * (P / i)][k] > A[h + j * (P / i) + pad][k] && A[h + j * (P / i)][Q] == MIN) || (A[h + j * (P / i)][k] < A[h + j * (P / i) + pad][k] && A[h + j * (P / i)][Q] == MAX)) {
+    //                     bitonic_swap(A[h + j * (P / i)], A[h + j * (P / i) + pad], k);
+    //                 }
+    //             }
+    //         }
+
+    //         while ((pad /= 2) >= 1) {
+    //             for (n = 0; n < P / (4 * i); n += pad) {
+    //                 for (m = 0; m < pad; m++) {
+    //                     for (k = 0; k < Q; k++) {
+    //                         if ((A[m + 2*n + j * (P / i)][k] > A[m + 2*n + pad + j * (P / i)][k] && A[m + 2*n + j * (P / i)][Q] == MIN) || (A[m + 2*n + j * (P / i)][k] < A[m + 2*n + pad + j * (P / i)][k] && A[m + 2*n + j * (P / i)][Q] == MAX)) {
+    //                             bitonic_swap(A[m + 2*n + j * (P / i)], A[m + 2*n + pad + j * (P / i)], k);
+    //                         } // PREPEI NA PROSTETHOUN OROI
+    //                     }
+    //                 }
+    //                 for (m = P / (2 * i); m < P / (2 * i) + pad; m++) {
+    //                     for (k = 0; k < Q; k++) {
+    //                         if ((A[m + 2*n + j * (P / i)][k] > A[m + 2*n + pad + j * (P / i)][k] && A[m + 2*n + j * (P / i)][Q] == MIN) || (A[m + 2*n + j * (P / i)][k] < A[m + 2*n + pad + j * (P / i)][k] && A[m + 2*n + j * (P / i)][Q] == MAX)) {
+    //                             bitonic_swap(A[m + 2*n + j * (P / i)], A[m + 2*n + pad + j * (P / i)], k);
+    //                         } // PREPEI NA PROSTETHOUN OROI
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //         for (s = 0; s < P / i; s += 2) {
+    //             if (A[s + 1][Q] == MAX) {
+    //                 qsort(A[s + j * (P / i)], Q, sizeof(int), asc_compare);
+    //                 qsort(A[s + j * (P / i) + 1], Q, sizeof(int), asc_compare);
+    //             } else {
+    //                 qsort(A[s + j * (P / i)], Q, sizeof(int), desc_compare);
+    //                 qsort(A[s + j * (P / i) + 1], Q, sizeof(int), desc_compare);
+    //             }
+    //         }
+
+    //         for (int x = 0; x < P; x++) {
+    //             for (int y = 0; y < Q; y++) {
+    //                 printf("%d ", A[x][y]);
+    //             }
+    //             printf("\n");
+    //         }
+    //         printf("\n");
+    //     }
+    // }
+
+    // printf("\n");
+    // for (i = 0; i < P; i++) {
+    //     for (j = 0; j < Q; j++) {
+    //         printf("%d ", A[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
 
     return 0;
+}
+
+int ipow(int base, int exp) {
+    int result = 1;
+
+    for (;;) {
+        if (exp & 1) {
+            result *= base;
+        }
+        exp >>= 1;
+        if (!exp) {
+            break;
+        }
+        base *= base;
+    }
+
+    return result;
 }
 
 int asc_compare(const void *a, const void *b) {
