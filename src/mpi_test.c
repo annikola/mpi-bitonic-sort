7d1f04a507d1fd10ad0441c58bd1e1184d71bb4d
annikola <annikolaprof@gmail.com>
2024-12-30 20:14:55 +0200

    Added vactor exchange to the MPI programm

--- src/mpi_test.c
+++ src/mpi_test.c
@@ -9,23 +9,30 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../include/vectoral_bitonic_sort.h"
 
 #define MASTER 0
 #define MIN_ARGS 2
 #define MIN_P 1
 #define MAX_P 7
-#define MIN_Q 20
+#define MIN_Q 2
 #define MAX_Q 27
 
+#define MAX_INTEGER 100000
+
 int log_base_2(int arg);
+int asc_compare(const void *a, const void *b);
+int desc_compare(const void *a, const void *b);
+void bitonic_swap(int *v1, int *v2, int idx);
 
 int main (int argc, char *argv[]) {
 
-    int i, j;
+    int t, i, j, k;
     int numtasks, taskid, len;
-    int p, q, total_proc, reps, total_reps;
+    int p, q, Q, total_proc, reps, total_reps;
     char hostname[MPI_MAX_PROCESSOR_NAME];
+    int *A, *B;
     Instruction **instructions;
 
     if (argc < MIN_ARGS + 1) {
@@ -44,39 +51,76 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
+    srand(time(0) + taskid);
+
+    Q = ipow(2, q);
     total_proc = ipow(2, p);
     reps = p + 1;
     total_reps = ((reps - 1) * reps) / 2;
     instructions = mpi_bitonic_warmup(total_proc, reps, total_reps);
 
-    // Print the Instructions "truth" table
-    for (j = 0; j < total_proc; j++) {
-        printf("For process %d\n", j);
-        printf("--------------\n");
-        for (i = 0; i < total_reps; i++) {
-            if (instructions[i][j].sort == ASCENT) {
-                printf("sort elements of %d ascending\n", j);
-            } else if (instructions[i][j].sort == DESCENT) {
-                printf("sort elements of %d descending\n", j);
-            }
-
-            if (!instructions[i][j].flow) {
-                printf("%d sends to %d\n", j, instructions[i][j].target_pid);
-            } else {
-                printf("%d receives from %d\n", j, instructions[i][j].target_pid);
-            }
-        }
-        printf("\n\n");
-    }
-
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-    MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
     MPI_Get_processor_name(hostname, &len);
     printf ("Hello from task %d on %s!\n", taskid, hostname);
-    if (taskid == MASTER) {
-        printf("MASTER: Number of MPI tasks is: %d\n",numtasks);
+    fflush(stdout);
+
+    B = (int *)malloc(Q * sizeof(int));
+    A = (int *)malloc(Q * sizeof(int));
+    for (i = 0; i < Q; i++) {
+        A[i] = rand() % MAX_INTEGER + 1;
     }
+    printf("%d HAS ", taskid);
+    for (int m = 0; m < Q; m++) {
+        printf("%d ", A[m]);
+    }
+    printf("\n");
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    for (i = 0; i < total_reps; i++) {
+        if (instructions[i][taskid].sort == ASCENT) {
+            printf("sort elements of %d ascending\n", taskid);
+            qsort(A, Q, sizeof(int), asc_compare);
+        } else if (instructions[i][taskid].sort == DESCENT) {
+            printf("sort elements of %d descending\n", taskid);
+            qsort(A, Q, sizeof(int), desc_compare);
+        }
+
+        if (!instructions[i][taskid].flow) {
+            MPI_Send(A, Q, MPI_INT, instructions[i][taskid].target_pid, 0, MPI_COMM_WORLD);
+            printf("%d SENT TO %d at %ld\n", taskid, instructions[i][taskid].target_pid, clock());
+            MPI_Recv(B, Q, MPI_INT, instructions[i][taskid].target_pid, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // printf("%d RECEIVED ", taskid);
+            // for (int m = 0; m < Q; m++) {
+            //     printf("%d ", B[m]);
+            // }
+            // printf("at %ld\n", clock());
+            // for (k = 0; k < Q; k++) {
+            //     if (A[k] > A[instructions[i][taskid].target_pid][k]) {
+            //         bitonic_swap(A[i], A[instructions[repetition][i].target_pid], k);
+            //     }
+            // }
+        } else {
+            MPI_Recv(B, Q, MPI_INT, instructions[i][taskid].target_pid, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // printf("%d RECEIVED ", taskid);
+            // for (int m = 0; m < Q; m++) {
+            //     printf("%d ", B[m]);
+            // }
+            // printf("at %ld\n", clock());
+            MPI_Send(A, Q, MPI_INT, instructions[i][taskid].target_pid, 0, MPI_COMM_WORLD);
+            printf("%d SENT TO %d at %ld\n", taskid, instructions[i][taskid].target_pid, clock());
+        }
+    }
+
+    printf("sort elements of %d ascending\n", taskid);
+    qsort(A, Q, sizeof(int), asc_compare);
+    printf("%d HAS ", taskid);
+    for (int m = 0; m < Q; m++) {
+        printf("%d ", A[m]);
+    }
+    printf("\n");
+
     MPI_Finalize();
 
     return 0;
@@ -96,4 +140,20 @@ int log_base_2(int arg) {
     }
 
     return result;
+}
+
+int asc_compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int desc_compare(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
+void bitonic_swap(int *v1, int *v2, int idx) {
+    int dummy;
+
+    dummy = v1[idx];
+    v1[idx] = v2[idx];
+    v2[idx] = dummy;
 }
